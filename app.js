240a311e3d1041396b03df799ae6fc7424c29026
Jonathan Jong <jonathanjong@jonathans-air.lan>
2022-08-23 09:36:50 -0500

    Remove node_modules/ from git staging index

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
+    
 });
 
 app.listen(process.env.PORT || 3000, () => {
